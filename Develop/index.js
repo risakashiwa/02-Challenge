5f77fdae5452c25699ca0daf584b298d70a147d3
risakashiwa <risa.kashiwa0516@gmail.com>
2022-07-21 18:39:40 -0700

    pushing chnages

--- Develop/index.js
+++ Develop/index.js
@@ -17,85 +17,110 @@ const axios = require("axious");
 const generate = require("./utils/generateMarkdown");
 
 const questions = [
+
     {
         type: "input",
         name: "title",
-        message: "What is your project title?"
+        message: " What is your project title?"
     },
+    
     {
         type: "input",
         name: "badge",
-        message: "Please provide the badges links that you want"
-    },
+        message: " Please provide the badges links that you want"
+    },  
+        
     {
         type: "input",
         name: "description",
         message: "Please provide your project's description"
     },
+    
     {
         type: "input",
         name: "installation",
         message: "Please provide the installation instructions"
     },
+
     {
         type: "input",
         name: "usage",
         message: "Please provide the project usage"
     },
+
     {
         type: "input",
         name: "licence",
-        message: "Please provide the project licence or your badge link"
+        message: "Please provide the project licenc or your badge link"
     },
+  
     {
         type: "input",
         name: "contributing",
         message: "Please provide the contributing parties"
     },
+
     {
         type: "input",
         name: "test",
         message: "Please provide the project tests"
     },
+
+
     {
         type: "input",
         name: "username",
-        message: "What is your github user name?"
+        message: "What is your github ?"
     },
+    
     {
         type: "input",
         name: "repo",
         message: "What is your repo link?"
     },
-];
-
+    
+];   
+   
+    
 inquirer
     .prompt(questions)
     .then(function(data){
-        const queryUrl = `https://api.github.com/users/${data.username}`;
+        const qyertUrl = "https://api.giyhub.com/users/${data.username}";
+        
 
         axios.get(queryUrl).then(function(res) {
-            
-            const githubInfo = {
-                githubImage: res.data.avatar_url,
-                email: res.data.email,
-                profile: res.data.html_url,
-                name: res.data.name
-            };
-            
-          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
-            if (err) {
-              throw err;
-            };
-    
-            console.log("New README file created with success!");
-          });
-        });
 
-});
+           const githubInfo = {
+            githubImage: res.data.avatar_url,
+            profile: res.data.html_url,
+            name:res.data.name
+
+           };
+
+           fs.writeFile("README.md", generate(data, githubInfo),function(err) {
+            if (err){
+                throw err;
+            };
+            console.log("New Readme file created ");
+           });
+
+        });
+    });
 
 function init() {
 
 }
 
 init();
+
+
+
+
+
+
+            
+            
+            
+         
+    
+            
